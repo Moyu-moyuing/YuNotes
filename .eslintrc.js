a8e3f3396affa6fa110f5627a3eb2416021acd2f
Moyu-moyuing <1531872149@qq.com>
2023-04-28 04:18:11 +0800

    ci: 增加prettier和eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true
     },
-    "extends": [
+    extends: [
         // "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:vue/vue3-recommended",
+        'plugin:@typescript-eslint/recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:prettier/recommended'
     ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
     },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    plugins: ['vue', '@typescript-eslint'],
+    rules: {}
 }
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    semi: false,//末尾逗号，default:true
+    singleQuote: true,// 单引号，default:false
+    trailingComma: "none",// 末尾分号,default:always
+    arrowParens: "avoid",//default:always
+    proseWrap: "never",
+    endOfLine: "auto",//default lf
+};
