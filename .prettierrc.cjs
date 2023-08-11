fd5c9a0241caecca0dd001c615931ea962b87099
Moyu-moyuing <1531872149@qq.com>
2023-08-11 16:00:07 +0800

    ci: 完善代码格式配置

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,8 +1,10 @@
 module.exports = {
+    printWidth: 100, // 超过最大值换行 
     semi: false,//末尾逗号，default:true
     singleQuote: true,// 单引号，default:false
     trailingComma: "none",// 末尾分号,default:always
-    arrowParens: "avoid",//default:always
+    arrowParens: "avoid",//default:always,为单行箭头函数的参数添加圆括号 
     proseWrap: "never",
-    endOfLine: "auto",//default lf
+    endOfLine: "auto",//default lf 设置统一的行结尾样式 保持现有的行尾 
+    bracketSpacing: true, // 格式化结果为 : { foo: bar } 
 };
